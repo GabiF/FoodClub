15a65d50d58dd19bac36e370cb6974db3190e9bf
Rares Urs <urs_rares96@gmail.com>
2018-09-13 12:35:40 -0700

    Hello World

---
+++ src/main/java/com/food/club/HelloController.java
@@ -0,0 +1,23 @@
+package com.food.club;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class HelloController {
+
+    private static long start = System.currentTimeMillis();
+
+    @GetMapping("/")
+    public String sayHello() {
+        long millis = System.currentTimeMillis() - start;
+        String uptime = String.format("%02d:%02d",
+                TimeUnit.MILLISECONDS.toMinutes(millis),
+                TimeUnit.MILLISECONDS.toSeconds(millis) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+        return String.format("Hello, Spring! (up %s, %s)", uptime, System.getenv("BUILD_PROFILE"));
+    }
+
+}
