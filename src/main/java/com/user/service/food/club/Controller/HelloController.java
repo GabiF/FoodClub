8688e0e43647833cc9dbf5795c90b07e1f563755
Rares Urs <urs_rares96@gmail.com>
2018-09-16 08:30:03 -0700

    Updated controller to return json rest request

--- src/main/java/com/user/service/food/club/Controller/HelloController.java
+++ src/main/java/com/user/service/food/club/Controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.user.service.food.club.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    private static long start = System.currentTimeMillis();
-
-    @GetMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
     public String sayHello() {
         return "Hello User";
     }
