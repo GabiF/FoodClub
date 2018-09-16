f6c275ffcd97eed1b38384d0f660f20ef20ebaef
Rares Urs <urs_rares96@gmail.com>
2018-09-16 08:00:23 -0700

    Renamed spring boot application

--- src/main/java/com/user/service/food/club/UserApplication.java
+++ src/main/java/com/user/service/food/club/UserApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
-
+public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
+        SpringApplication.run(UserApplication.class, args);
     }
 }
