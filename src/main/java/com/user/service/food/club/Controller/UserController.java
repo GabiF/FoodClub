43a63ba853b12cf2ab255e9b85126cc918fa0e89
Rares Urs <urs_rares96@gmail.com>
2018-09-17 16:20:31 -0700

    Added update user details

--- src/main/java/com/user/service/food/club/Controller/UserController.java
+++ src/main/java/com/user/service/food/club/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.user.service.food.club.Entity.User;
 import com.user.service.food.club.Exception.UserNotFoundException;
 import com.user.service.food.club.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +20,13 @@ public class UserController {
         return "Welcome Page";
     }
 
-    @RequestMapping(value = "/getUsers", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/users", method = RequestMethod.GET, produces = "application/json")
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
     }
 
 
-    @GetMapping("/students/{id}")
+    @GetMapping("/users/{id}")
     public User retrieveStudent(@PathVariable long id) {
         User user = userRepository.findById(id);
         if (user == null) {
@@ -44,4 +45,15 @@ public class UserController {
         userRepository.delete(id);
     }
 
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Object> updateStudent(@RequestBody User user, @PathVariable Long id) {
+        User userOptional = userRepository.findById(id);
+
+        if (userOptional == null)
+            throw new UserNotFoundException("The user you want to update cannot be found: " + id);
+
+        userOptional.setId(id);
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
+    }
 }
